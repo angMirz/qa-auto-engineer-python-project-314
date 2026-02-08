80bc5f408c037c9c6e69503ccea0d303d164cd91
angelina <m.angelina.qa@gmail.com>
2026-02-08 22:59:06 +0300

    upd test

--- tests/test_users.py
+++ tests/test_users.py
@@ -106,4 +106,9 @@ def test_delete_all_users(driver, base_url, logged_in):
     assert len(users) == 0, "Список не пуст"
 
     #Негатив
-    page.all_delete_users_form()
+    try:
+        page.all_delete_users_form()
+        snackbar = page.snackbar_text()
+        assert "elements deleted" in snackbar, "Негативный кейс: список уже пуст"
+    except Exception as e:
+        raise AssertionError("Негативный кейс: список пуст, удалять нечего") from e
